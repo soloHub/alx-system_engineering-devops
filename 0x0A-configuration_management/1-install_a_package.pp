0b182c22e631c7592b4dccd3353ff63f11cddc7f
Solomon Kareem <solomon.kareem@gmail.com>
2024-04-25 13:08:05 +0000

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install flask from pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Define a class for installing Flask
+class { 'flask':
+  # Ensure pip3 is installed (adjust package name if needed)
+  Package['python3-pip']
+
+  # Install Flask version 2.1.0 using pip3
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/usr/local/bin'], # Adjust paths if pip3 is elsewhere
+  }
 }
